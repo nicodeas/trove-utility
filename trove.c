ecaf4909464f83f04c5ba1fde33cf5b6faf75c63
ChantelleKerr <chantellekerr93@gmail.com>
2022-10-07 15:00:24 +0800

    Working makefile

--- trove.c
+++ trove.c
@@ -1,38 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-// cc -std=c11 -Wall -Werror -o trove trove.c
+#include "trove.h"
 
-void store_path(char *path) {
-  FILE *fp;
-  // TODO: Make sure trove-file is not hard coded.
-  fp = fopen("trove-file", "a");
-
-  if(fp == NULL) {
-    //perror(path);
-    exit(EXIT_FAILURE);
-  }
-  // Append path to file
-  fprintf(fp, "%s\n", path);
-  fclose(fp);
-}
-
-void get_path(char *filename){
-char buf[PATH_MAX]; 
-    char *res = realpath(filename, buf);
-    if (res != NULL) { 
-        printf("REAL PATH: %s \n", buf);
-        store_path(buf);
-    } else {
-        perror("realpath");
-        exit(EXIT_FAILURE);
-    }
-}
 int main(int argc, char *argv[]) {
-  printf("HEllo");
+  // Handle options here
 
+  // Currently it gets the filenames from the command line arg
+  // e.g. ./trove trove.h Makefile build.o build.c
   for(int i = 1; i < argc; i++) {
     get_path(argv[i]);
   }
-  
 }
