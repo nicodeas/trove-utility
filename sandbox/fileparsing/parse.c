ef4aeb6321f47c4a880b7ce47313d0177048a411
nicodeas <nicholas@theugroup.co>
2022-10-08 11:20:24 +0800

    add sample data to sandbox

--- sandbox/fileparsing/parse.c
+++ sandbox/fileparsing/parse.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <limits.h> /* PATH_MAX */
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 int word_length;
 
@@ -52,15 +56,56 @@ void parse_file(char *fname)
     }
 }
 
+void parse_fileargs(char *file_arg)
+{
+    char base_path[PATH_MAX];
+    char path[PATH_MAX];
+    struct stat file_stat;
+    realpath(file_arg, base_path);
+    stat(base_path, &file_stat);
+    if (S_ISDIR(file_stat.st_mode))
+    {
+        // printf("==========Opening directory: %s ==========\n", file_arg);
+        DIR *dirp;
+        struct dirent *dp;
+        dirp = opendir(file_arg);
+        if (dirp == NULL)
+        {
+            perror(file_arg);
+            exit(EXIT_FAILURE);
+        }
+        while ((dp = readdir(dirp)) != NULL)
+        {
+            // printf("%s\n", dp->d_name);
+            if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, ".."))
+            {
+                strcpy(path, base_path);
+                strcat(path, "/");
+                strcat(path, dp->d_name);
+                parse_fileargs(path);
+            }
+        }
+        // printf("==========Closing directory: %s ==========\n", file_arg);
+        closedir(dirp);
+    }
+    else
+    {
+        parse_file(file_arg);
+        // printf("%s\n", file_arg);
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    if (argc < 3)
     {
-        printf("%s: [filename] [wordlength]\n", argv[0]);
+        printf("%s: [wordlenght] [filename(s)] \n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    char *fname = argv[1];
-    word_length = atoi(argv[2]);
-    parse_file(fname);
+    word_length = atoi(argv[1]);
+    for (int i = 2; i < argc; i++)
+    {
+        parse_fileargs(argv[i]);
+    }
     exit(EXIT_SUCCESS);
 }
--- sandbox/realpath.c
+++ sandbox/realpath.c
@@ -2,15 +2,37 @@
 #include <limits.h> /* PATH_MAX */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-int main(int argc, char *argv[]) {
-    char buf[PATH_MAX]; 
-    char *res = realpath(argv[1], buf);
-    if (res != NULL) { 
-        printf("REAL PATH: %s \n", buf);
-    } else {
+int is_dir(char *path)
+{
+    struct stat file_stat;
+    stat(path, &file_stat);
+    return S_ISREG(file_stat.st_mode);
+}
+
+int main(int argc, char *argv[])
+{
+    char path[PATH_MAX];
+    if (argc < 2)
+    {
+        printf("Incorrect arguments\n");
+        exit(EXIT_FAILURE);
+    }
+    char *res = realpath(argv[1], path);
+    if (res != NULL)
+    {
+        printf("REAL PATH: %s \n", path);
+    }
+    else
+    {
         perror("realpath");
         exit(EXIT_FAILURE);
     }
+    int x = is_dir(path);
+    printf("%i\n", x);
+
     return 0;
 }
