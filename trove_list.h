c1cf0eb4a5e64a5fd5c9b213c145e8796fe36bd0
nicodeas <nicholas@theugroup.co>
2022-10-09 12:39:38 +0800

    add methods to trove list headers

---
+++ trove_list.h
@@ -0,0 +1,35 @@
+#include <stdbool.h>
+
+// ---------------------------------------
+// | Cat | PATH1 | PATH2 | PATH3 | PATH4 |
+// ---------------------------------------
+// -------------------------------------------------------
+// | Dog | PATH1 | PATH2 | PATH3 | PATH4 | PATH5 | PATH6 |
+// -------------------------------------------------------
+// -------------------------
+// | Apple | PATH1 | PATH2 |
+// -------------------------
+// ----------------
+// | Pear | PATH1 |
+// ----------------
+
+typedef struct _path_link
+{
+    char *filepath;
+    struct _path_link *next;
+} PATH_LINK;
+
+typedef struct _head_link
+{
+    char *word;
+    struct _head_link *next;
+    PATH_LINK *path_list;
+} HEAD_LINK;
+
+extern PATH_LINK new_path_link();
+extern bool path_exists(HEAD_LINK *head, char *path);
+extern void free_path_link(PATH_LINK *path_link);
+
+extern HEAD_LINK new_head_link();
+extern HEAD_LINK find_head_link(HEAD_LINK *head, char *word);
+extern void free_head_link(HEAD_LINK *head);
