b4a88e5b5b0223397324c1a6bbe0302d60719a25
nicodeas <nicholas@theugroup.co>
2022-10-09 11:17:57 +0800

    update trove headers

--- trove.h
+++ trove.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <stdbool.h>
 
 // ---------------------------------------
 // | Cat | PATH1 | PATH2 | PATH3 | PATH4 |
@@ -28,9 +29,9 @@ typedef struct _head_link
     PATH_LINK *path_list;
 } HEAD_LINK;
 
-extern void new_path_link();
-extern void path_exists(HEAD_LINK *head, char *path);
+extern PATH_LINK new_path_link();
+extern bool path_exists(HEAD_LINK *head, char *path);
 
-extern void new_head_link();
+extern HEAD_LINK new_head_link();
+extern HEAD_LINK find_head_link(HEAD_LINK *head, char *word);
 extern void free_head_link(HEAD_LINK *head);
-extern void find_head_link(HEAD_LINK *head, char *word);
