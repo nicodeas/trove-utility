c531e5ded6b9a2d7db41ada39bf13bd331d636c1
nicodeas <nicholas@theugroup.co>
2022-10-09 11:05:51 +0800

    add types to trove header

--- trove.h
+++ trove.h
@@ -2,5 +2,19 @@
 #include <stdlib.h>
 #include <limits.h>
 
+typedef struct _path_link
+{
+    char *filepath;
+    struct _path_link *next;
+} PATH_LINK;
+
+typedef struct _head_link
+{
+    char *word;
+    struct _head_link *next;
+    PATH_LINK *path_list;
+
+} HEAD_LINK;
+
 extern void store_path(char *path);
 extern void get_path(char *filename);
