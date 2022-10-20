5e97e7001fcb207e3224edbe521eb1cca6057403
ChantelleKerr <chantellekerr93@gmail.com>
2022-10-20 22:32:50 +0800

    added  back into hash table in remove

--- remove.c
+++ remove.c
@@ -6,8 +6,43 @@
 
 #include "remove.h"
 
+
+// void process_line(char *line, char *path, HASHTABLE *hashtable)
+// {
+
+//     char delimter[2] = {" "};
+//     char *token;
+//     token = strtok(line, delimter);
+//     while (token != NULL)
+//     {
+//         if (strlen(token) >= word_length)
+//         {
+//             printf("-- %s\n", token);
+//             HEAD_LINK *head = hashtable_find(hashtable, token);
+//             if (head == NULL)
+//             {
+//                 head = hashtable_add(hashtable, token);
+//             }
+
+//             LINK *ltp_cpy = head->link_to_paths;
+//             if (!path_link_find(ltp_cpy, path))
+//             {
+//                 LINK *path_link = new_link(path);
+//                 path_link->next = head->link_to_paths;
+//                 head->link_to_paths = path_link;
+                
+//             }
+            
+//         }
+//         token = strtok(NULL, delimter);
+        
+//     }
+// }
+
 void remove_file(char *trovefile, char *paths[], int path_count)
 {
+
+  HASHTABLE *hashtable = hashtable_new();
   int fd[2];
   if (pipe(fd) == -1)
   {
@@ -32,44 +67,91 @@ void remove_file(char *trovefile, char *paths[], int path_count)
   FILE *stream;
   stream = fdopen(fd[0], "r");
   char line[BUFSIZ];
-  char *prev_string = NULL;
-  bool prev_is_word = false;
-  bool remove_path = false;
-  FILE *fp = fopen("test.txt", "w");
+  //char *prev_string = NULL;
+  //bool prev_is_word = false;
+  //bool remove_path = false;
+  //FILE *fp = fopen("test.txt", "w");
   
-  
+  HEAD_LINK *head = NULL;
   while (fgets(line, BUFSIZ, stream) != NULL)
   {
-    free(prev_string);
-    prev_string = strdup(line);
-    if (*line == '#')
-    {
-      if (!prev_is_word)
-      {
-        fprintf(fp, "%s", prev_string);
+      if(*line == '#') {
+        memmove(line, line+1, strlen(line)); // Remove the hash
+        head = hashtable_add(hashtable, line);
+        printf("%s\n", head->word);
       }
-      prev_is_word = true;
-    }
-    else
-    {
-      for (int i = 0; i < path_count; i++)
-      {
-        char *p = basename(prev_string);
-        int len = strlen(p);
-        p[len -1] = '\0';
-        if (strcmp(paths[i], p) == 0)
+      else {
+        for (int i = 0; i < path_count; i++)
         {
-          remove_path = true;
-          break;
+          if(strstr(line, paths[i]) == NULL) {
+              LINK *path_link = new_link(line);
+              path_link->next = head->link_to_paths;
+              head->link_to_paths = path_link;
+              printf("%s\n", path_link->path);
+          }
+       
         }
       }
-      if (!remove_path)
-      {
-        prev_is_word = false;
-        fprintf(fp, "%s", prev_string);
-      }
-    }
   }
+  //   free(prev_string);
+  //   prev_string = strdup(line);
+  //   if (*line == '#')
+  //   {
+  //     if (!prev_is_word)
+  //     {
+  //       fprintf(fp, "%s", prev_string);
+  //     }
+  //     prev_is_word = true;
+  //   }
+  //   else
+  //   {
+  //     for (int i = 0; i < path_count; i++)
+  //     {
+  //       // char abs_path[PATH_MAX];
+  //       // realpath(abs_path, paths[i]);
+  //       // printf("PATH: %s\n", paths[i]);
+  //       //  struct stat file_stat;
+  //       // stat(abs_path, &file_stat);
+       
+  //       // if (S_ISDIR(file_stat.st_mode))
+  //       // {
+  //       //   printf("IM A DIR");
+
+
+  //       // }
+  //       // char *p = basename(prev_string);
+  //       //printf("--- %s\n", p);
+  //       // int len = strlen(p);
+  //       // p[len -1] = '\0';
+  //       // if (strcmp(paths[i], p) == 0)
+  //       // {
+  //       //   remove_path = true;
+  //       //   break;
+  //       // }
+  //       printf("-- %s -- %s --", paths[i], prev_string);
+  //       char *ret;
+  //       int len = strlen(paths[i]);
+        
+  //       ret = strstr(prev_string, paths[i]);
+  //       printf("^^ %i -- %c \n", len, ret[len]);
+  //       ret[len + 1] = '\0';
+  //       printf("-- %s -- %s -- %s\n", paths[i], prev_string, ret);
+  //       // if (strcmp(ret, paths[i]) == 0)
+  //       // {
+  //       //   printf("TESTING");
+  //       //   remove_path = true;
+  //       //   break;
+  //       // }
+  //     }
+  //     if (!remove_path)
+  //     {
+  //       prev_is_word = false;
+  //       fprintf(fp, "%s", prev_string);
+  //     }
+  //   }
+  // }
+
+  
 
   close(fd[0]);
 }
